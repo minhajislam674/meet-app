7510326474bc4901a79866952e4bf409a13c81f5
Minhaj Islam <minhajislam674@gmail.com>
2022-12-08 21:38:38 +0100

    Add unit tests for "search event by city" feature

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,10 +2,11 @@ import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
 import Event from '../Event';
+import { mockData } from "../mock-data";
 
 describe('<EventList/> component', () => {
     test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={[{id: 1}, {id: 2}, {id: 3}, {id: 4}]} />); //The prop 'events' contains four empty objects.
-        expect(EventListWrapper.find(Event)).toHaveLength(4); //This test will only pass if EventList renders exactly four events from its prop events.
+        const EventListWrapper = shallow(<EventList events={mockData} />); //The prop 'events' contains four empty objects.
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length); //This test will only pass if EventList renders exactly four events from its prop events.
     });
 })
